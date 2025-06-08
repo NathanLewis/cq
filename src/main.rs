e328d426137cddad3d0c75a77776188a102f5b8f
Nathan Lewis <nathanlewis42@gmail.com>
2025-06-08 09:43:33 +0100

    Added the index option and gave up for now on changing the help flag

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,11 @@ use clap::{Parser, ArgAction};
 use csv::Reader;
 
 #[derive(Parser, Debug)]
-#[command(version, about, long_about = None, disable_help_flag(true))]
+// #[command(version, about, long_about = None, disable_help_flag(true))]
+#[command(version, about, long_about = None)]
 struct Args {
-    #[arg(short = '?', action = ArgAction::Help)]
-    help: bool,
+    // #[arg(short = '?', action = ArgAction::Help)]
+    // usage: bool,
     
     #[arg(short, long, value_parser = clap::value_parser!(String))]
     file: String,
@@ -26,41 +27,61 @@ struct Args {
     // if true output the record count
     #[arg(short, long, default_value_t = false)]
     count: bool,
+
+    #[arg(short, long, default_value_t = false)]
+    eader: bool,
+    
+    #[arg(short, long, default_value_t = -1)]
+    index: i32
 }
 
-// The `main` function is where your program starts executing.
+
 fn main() {
     let args = Args::parse();
     println!("{:?}", args);
     let mut rdr: Reader<Box<dyn io::Read>>;
+    // Read from stdin
     if args.file == "" || args.file == "-" {
         rdr = csv::ReaderBuilder::new()
             .delimiter(args.delimiter.as_bytes()[0])
             .flexible(true)
             .from_reader(Box::new(io::stdin()));
     } else {
-        rdr = csv::ReaderBuilder::new()
-            .delimiter(args.delimiter.as_bytes()[0])
-            .flexible(true)
-            .from_reader(Box::new(match File::open(&args.file) {
+        // or read from a file
+        let file = match File::open(&args.file) {
             Ok(x) => x,
             Err(e) => {
                 eprintln!("Error opening file '{}': {}", args.file, e);
                 std::process::exit(1);
             }
-        }));
+        };
+        rdr = csv::ReaderBuilder::new()
+            .delimiter(args.delimiter.as_bytes()[0])
+            .flexible(true)
+            .from_reader(Box::new(file));
     }
     if args.count {
         let count = rdr.records().count();
         println!("{:?} records", count);
         return;     
     }
-    // Loop over each record.
-    for result in rdr.records() {
-        // An error may occur, so abort the program in an unfriendly way.
-        // We will make this more friendly later!
-        let record = result.expect("a CSV record");
-        // Print a debug version of the record.
-        println!("{:?}", record);
+    
+    if args.index > -1 {
+        let index = args.index as usize;
+        // Loop over each record.
+        for result in rdr.records() {
+            // An error may occur, so abort the program in an unfriendly way.
+            // We will make this more friendly later!
+            let record = result.expect("a CSV record");
+            println!("{}", match record.get(index) {
+                Some(x) => x,
+                None => todo!(),
+            }.trim_end());
+        }
+    } else { 
+        for result in rdr.records() {
+            let record = result.expect("a CSV record");
+            println!("{:?}", record);
+        }
     }
 }
